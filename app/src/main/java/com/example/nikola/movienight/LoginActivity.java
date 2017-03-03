c042dfc7b0a63db424b6d23afc07887065fbabab
MenloB <nik_jov_92@hotmail.com>
2017-03-03 18:03:00 +0100

    Realizovana logika za sign in i dodat dummy content za MoviesActivity

--- app/src/main/java/com/example/nikola/movienight/LoginActivity.java
+++ app/src/main/java/com/example/nikola/movienight/LoginActivity.java
@@ -1,11 +1,21 @@
 package com.example.nikola.movienight;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,10 +24,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button   signin;
     private Button   signup;
 
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         email    = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
@@ -29,12 +43,43 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         signup.setOnClickListener(this);
     }
 
+    private void toastShow(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getIn() {
+        Intent moviesIntent = new Intent(this, MoviesActivity.class);
+        startActivity(moviesIntent);
+        finish();
+    }
+
+    private void signIn(String mail, String pass) {
+        firebaseAuth.signInWithEmailAndPassword(mail, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    getIn();
+                }
+            }
+        });
+    }
+
     @Override
     public void onClick(View view) {
         if(view == signup) {
             Intent mainAct = new Intent(this, MainActivity.class);
             startActivity(mainAct);
             finish();
+        } else if (view == signin) {
+            String mail = email.getText().toString().trim();
+            String pass = password.getText().toString().trim();
+            if(TextUtils.isEmpty(mail)) {
+                toastShow("Provide us with you email.");
+            } else if(TextUtils.isEmpty(pass)) {
+                toastShow("Provide us with you password.");
+            } else {
+                signIn(mail, pass);
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/nikola/movienight/MoviesActivity.java
@@ -0,0 +1,48 @@
+package com.example.nikola.movienight;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    private TextView resp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+
+        resp = (TextView) findViewById(R.id.resp);
+
+        RequestQueue rq = Volley.newRequestQueue(this);
+        String url = "https://api.themoviedb.org/4/list/10?page=1&api_key=8e20230f25939a349c2e37680cdaff95&sort_by=release_date.asc";
+
+        StringRequest sr = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // Display the first 500 characters of the response string.
+                resp.setText(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                resp.setText("Nije moguce kontaktirati servis");
+            }
+        });
+// Add the request to the RequestQueue.
+        rq.add(sr);
+    }
+}
